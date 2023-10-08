53423d7a306ef842e363e83e3fac94436ca27631
hyeongbin <gudqls1539@gmail.com>
2023-10-08 19:37:54 +0900

    version #2

--- db_info.py
+++
@@ -1,7 +0,0 @@
-host = "billdb-dev.clb8vfq4410k.ap-northeast-2.rds.amazonaws.com"
-port = 33066
-user = "billadmin"
-passwd = "billadmin"
-db = "billdbdev"
-
-google_chat_webhook_url = 'https://chat.googleapis.com/v1/spaces/AAAABU-GaUU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=nYrcKWSxmZNIPP8loDDRS9mHBr90eBBCcQ86rH3ebOI'
